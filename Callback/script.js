1a089e26ecd3be9f24cda3cfae550ed56fcf4953
DFR1991 <diegoriveros1991@gmail.com>
2022-08-05 16:12:08 -0300

    first commit

--- Callback/script.js
+++ Callback/script.js
@@ -1,11 +1,9 @@
 
-function gastoConIva(costo){
+const gastoConIva = (num)=>{
 
-    let monto = Number(costo);
-
-    return console.log(`Total Iva: ${monto + monto *0.21}`); 
+    return console.log(`Total Iva: ${num + num *0.21}`); 
 }
 
-let nums = [100, 150, 200, 250];
+let nums = [100, 150, 200, 250, 300];
 
 let MontosConIva = nums.map(gastoConIva);
