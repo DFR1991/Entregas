5ca59b671920063ee62f3e17c918c60df29beab2
DFR1991 <diegoriveros1991@gmail.com>
2022-08-20 22:54:11 -0300

    Revert "entrega 2"
    This reverts commit beb17a4bdaf6fe40c682a88d8f0a29462708759a.

--- Expresiones_Regulares/script.js
+++ Expresiones_Regulares/script.js
@@ -1,8 +1,7 @@
 const pass = document.querySelector('#password');
 const pattern = /^(?![\s])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=\w).\S{11,19}$/;
-
-    document.querySelector('#submit').addEventListener('click', (e)=> {
-        e.preventDefault();
-        let password = pass.value;
-        password.match(pattern) ? console.log('Password válido') :  console.log('Password inválido');
-    });
+document.querySelector('#submit').addEventListener('click', (e)=> {
+    e.preventDefault();
+    let password = pass.value;
+    password.match(pattern) ? console.log('Password válido') :  console.log('Password inválido');
+});
